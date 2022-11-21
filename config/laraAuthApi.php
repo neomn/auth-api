9ae656ed7ead8dc2913d8231417a10d34afbad71
neomn110 <neomn110@gmail.com>
2022-11-21 08:18:57 +0330

    feat: publish laravelAuthApi config file

--- config/laraAuthApi.php
+++ config/laraAuthApi.php
@@ -1,4 +1,28 @@
 <?php
+
 return [
 
+// set registration method to true/false to set them enable/disable
+    $registrationMethods = [
+        'phone_number' => true,
+        'email' => true,
+        'username' => true,
+        'social_media' => true,
+    ],
+
+    $loginMethods = [
+        'username' => true,
+        'email' => true,
+        'phone_number' => true,
+        'social_media' => true,
+    ],
+
+    $twoFactorAuthMethod = [
+        'phone_number' => true,
+        'email' => true,
+        'google_authenticator' => true,
+    ],
+
+    // force every single user to use two-factor authentication
+    $mandatoryTwoFactorAuthentication = false,
 ];
